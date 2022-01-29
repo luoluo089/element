2ea5d759c56441d106f4d366d544b2ef34a8afbc
itcast <727261722@qq.com>
2022-01-29 11:20:33 +0800

    提交到远程仓库

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//使用element-ui
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
